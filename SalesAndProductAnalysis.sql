a5cdc54744842e21fca3e580d29374583d464827
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-01-30 22:49:44 -0500

    Added second sql

--- SalesAndProductAnalysis.sql
+++ SalesAndProductAnalysis.sql
@@ -22,5 +22,45 @@ GO
 --Long-Sleeve Logo Jersey (~$200k) and Sport-100 Helmet (~$165k).
 
 SELECT *
-FROM Sales.SalesOrderDetail
+FROM Sales.SalesOrderHeader
 --FROM Production.Product
+
+SELECT *
+FROM Production.Product 
+GO
+
+
+-- 2011-05-31 00:00:00.000 ~ 2014-06-30 00:00:00.000
+WITH Sales AS (
+SELECT CustomerID
+	, SUM(TotalDue) AS TotalSpending
+	, MIN(OrderDate) OVER (PARTITION BY CustomerID) AS FirstDate
+	, MAX(OrderDate) OVER (PARTITION BY CustomerID) AS LastDate
+	, DATEDIFF(DAY, 
+		MIN(OrderDate) OVER (PARTITION BY CustomerID), 
+		DATEADD(day, 1, MAX(OrderDate) OVER (PARTITION BY CustomerID))
+	) AS ActiveDays
+FROM Sales.SalesOrderHeader
+--WHERE YEAR(OrderDate) IN (2013, 2014)
+--GROUP BY CustomerID
+),
+Customers AS (
+SELECT c.CustomerID
+	, CONCAT(ISNULL(p.Title + ' ', ''), p.FirstName, ' ', p.LastName) AS FullName
+FROM Sales.Customer c
+	INNER JOIN Person.Person p
+		ON c.PersonID = p.BusinessEntityID
+)
+SELECT Sales.*
+	, Customers.FullName
+FROM Sales
+INNER JOIN Customers
+ON Sales.CustomerID = Customers.CustomerID
+
+
+SELECT soh.SalesOrderID, sod.ProductID, sod.OrderQty, soh.OrderDate,
+    DATEDIFF(day, MIN(soh.OrderDate)   
+        OVER(PARTITION BY soh.SalesOrderID), SYSDATETIME()) AS 'Total'  
+FROM Sales.SalesOrderDetail sod  
+    INNER JOIN Sales.SalesOrderHeader soh  
+        ON sod.SalesOrderID = soh.SalesOrderID  
