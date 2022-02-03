7be45757617514da51fd9b77236a35f5c516ad0a
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-02-03 00:42:17 -0500

    Added Q3 to get the top valuable customers based on RFM model

--- SalesAndProductAnalysis.sql
+++ SalesAndProductAnalysis.sql
@@ -32,7 +32,6 @@ WITH RawSales AS (
 		, MIN(OrderDate) AS FirstDate
 		, MAX(OrderDate) AS LastDate
 		, DATEDIFF(DAY, MIN(OrderDate), DATEADD(DAY, 1, MAX(OrderDate))) AS ActiveDays
-		, CAST('2014-07-01T00:00:00.000' AS datetime) AS CurrentDate
 	FROM Sales.SalesOrderHeader
 	GROUP BY CustomerID
 ),
@@ -45,22 +44,44 @@ Customers AS (
 )
 SELECT RawSales.*
 	, ActiveWeeks =
-	CASE
-		WHEN CAST(RawSales.ActiveDays / 7 AS INT) = 0 THEN 1
-		ELSE CAST(RawSales.ActiveDays / 7 AS INT)
-	END
-	--, CAST(RawSales.ActiveDays / 7 AS INT) AS ActiveWeeks
-	, DATEDIFF(DAY, RawSales.LastDate, RawSales.CurrentDate) AS DaysSinceLastTxn
+		CASE
+			WHEN CAST(RawSales.ActiveDays / 7 AS INT) = 0 THEN 1
+			ELSE CAST(RawSales.ActiveDays / 7 AS INT)
+		END
+	, DATEDIFF(DAY, RawSales.LastDate, '2014-07-01T00:00:00.000') AS DaysSinceLastTxn
 	, Customers.FullName
 FROM RawSales
 	INNER JOIN Customers
 		ON RawSales.CustomerID = Customers.CustomerID;
 GO
 
--- Q3 - Individual Customer Segmentation
+-- Q3 - Individual Customer Segmentation with RFM Model
 WITH CusSales AS (
 SELECT CustomerID
 	, FullName
-	, NumOfTxn / ActiveWeeks AS WeeklyTxnNum
+	, CAST(NumOfTxn / ActiveWeeks AS FLOAT) AS WeeklyTxnNum
 	, TotalSpending / ActiveWeeks AS WeeklySpending
-FROM dbo.vCustomerRFM
+	, DaysSinceLastTxn
+FROM vCustomerRFM
+),
+CusRank AS (
+SELECT CusSales.*
+--CustomerID
+--	, FullName
+	, NTILE(5) OVER (ORDER BY DaysSinceLastTxn) AS RankRecency
+	, NTILE(5) OVER (ORDER BY WeeklyTxnNum DESC) AS RankWeeklyTxnNum
+	, NTILE(5) OVER (ORDER BY WeeklySpending DESC) AS RankWeeklySpending
+FROM CusSales
+),
+CusRFM AS (
+SELECT CusRank.*
+	, CONCAT(RankRecency, RankWeeklyTxnNum, RankWeeklySpending) AS RFM
+	, RankRecency + RankWeeklyTxnNum + RankWeeklySpending AS RFMSum
+FROM CusRank
+)
+SELECT * 
+FROM CusRFM
+WHERE RFM = 111
+ORDER BY RankWeeklySpending DESC;
+GO
+
